fc17508ac79b4bf3aa4bce65cc39ba17af6a2f32
tomas-radic <tomas.radic@gmail.com>
2020-11-14 12:07:18 +0100

    Changed ownership checkbox

--- app/controllers/users/ownerships_controller.rb
+++ app/controllers/users/ownerships_controller.rb
@@ -1,5 +1,10 @@
 class Users::OwnershipsController < Users::BaseController
 
+  def index
+    @records = current_user.ownerships
+    @raws = Raw.all
+  end
+
   def create
     @raw = Raw.find(params[:raw_id])
     current_user.ownerships.where(raw: @raw).first_or_create
