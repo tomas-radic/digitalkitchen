f60b5a255b64b16b147571a5ad6da0375b3cd615
tomas-radic <tomas.radic@gmail.com>
2020-11-29 15:06:04 +0100

    Fixed food policy

--- app/policies/food_policy.rb
+++ app/policies/food_policy.rb
@@ -2,7 +2,7 @@ class FoodPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      scope.where("owner_private is false or owner_id = ?", user.id)
+      scope.where("owner_private is false or owner_id = ?", user&.id)
     end
   end
 
