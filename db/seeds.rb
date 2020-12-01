b325747809bb7796a5152e10a42a029be82a2153
tomas-radic <tomas.radic@gmail.com>
2020-12-01 17:31:51 +0100

    Random foods commented out in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,55 +45,55 @@ ActiveRecord::Base.transaction do
   end
 
 
-  # ------------------ Random foods generator ------------------
-  puts "\nAdding foods..."
-  foods_to_generate = 32
-
-  5.times do
-    FoodCategory.where(name: Faker::Lorem.words(number: 1..2).join(' ')).first_or_create!
-  end
-
-  foods_to_generate.times do
-    name = Faker::Food.dish
-
-    Food.create!(
-        name: name,
-        food_category: FoodCategory.all.sample,
-        owner: User.all.sample
-    ).tap do |food|
-
-      rand(1..3).times do
-        food.parts.create!(
-            name: Faker::Lorem.words(number: 1..3).join(' '),
-            description: Faker::Lorem.sentences(number: 5..20).join(' ')
-        ).tap do |part|
-
-          ingredients_count = rand(4..8)
-
-          ingredients_count.times do
-            optional = rand(1..10) > 8
-            part.ingredients.create!(optional: optional).tap do |ingredient|
-              n = rand(1..10)
-              alternatives_count = if n > 9
-                                     3
-                                   elsif n > 8
-                                     2
-                                   else
-                                     1
-                                   end
-
-              alternatives_count.times do
-                Alternative.create!(
-                    ingredient: ingredient,
-                    raw: Raw.all.sample
-                )
-              end
-            end
-          end
-        end
-      end
-    end
-  end
+  # # ------------------ Random foods generator ------------------
+  # puts "\nAdding foods..."
+  # foods_to_generate = 32
+  #
+  # 5.times do
+  #   FoodCategory.where(name: Faker::Lorem.words(number: 1..2).join(' ')).first_or_create!
+  # end
+  #
+  # foods_to_generate.times do
+  #   name = Faker::Food.dish
+  #
+  #   Food.create!(
+  #       name: name,
+  #       food_category: FoodCategory.all.sample,
+  #       owner: User.all.sample
+  #   ).tap do |food|
+  #
+  #     rand(1..3).times do
+  #       food.parts.create!(
+  #           name: Faker::Lorem.words(number: 1..3).join(' '),
+  #           description: Faker::Lorem.sentences(number: 5..20).join(' ')
+  #       ).tap do |part|
+  #
+  #         ingredients_count = rand(4..8)
+  #
+  #         ingredients_count.times do
+  #           optional = rand(1..10) > 8
+  #           part.ingredients.create!(optional: optional).tap do |ingredient|
+  #             n = rand(1..10)
+  #             alternatives_count = if n > 9
+  #                                    3
+  #                                  elsif n > 8
+  #                                    2
+  #                                  else
+  #                                    1
+  #                                  end
+  #
+  #             alternatives_count.times do
+  #               Alternative.create!(
+  #                   ingredient: ingredient,
+  #                   raw: Raw.all.sample
+  #               )
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 
 
   puts "\nDone."
